8aa186a4c5a91523e3080513a4eb5d0d5946c114
jeremyholbrecht <Griffley@proton.me>
2024-01-04 13:50:23 +0100

    added some data

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 // insert your token
 func main() {
-	rotomdex, err := discordgo.New("Bot ")
+	rotomdex, err := discordgo.New("Bot")
 
 	if err != nil {
 		log.Fatal(err)
@@ -94,4 +94,105 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		s.ChannelMessageSendEmbed(m.ChannelID, &bulbasaur)
 	}
+
+	if m.Content == "ivysaur" {
+		ivysaur := discordgo.MessageEmbed{
+			Title:     "Ivysaur",
+			Timestamp: time.Now().Format(time.RFC3339),
+			Color:     0x00ff00, //green
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://archives.bulbagarden.net/media/upload/c/c1/002Ivysaur_RB.png",
+			},
+
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "DexNr",
+					Value:  "0002",
+					Inline: true,
+				},
+
+				{
+					Name:   "Type",
+					Value:  "Grass/Poison",
+					Inline: true,
+				},
+
+				{
+					Name:   "Species",
+					Value:  "Seed Pokémon",
+					Inline: true,
+				},
+
+				{
+					Name:   "Abilities",
+					Value:  "Overgrow/Chlorophyll(Hidden Ability)",
+					Inline: false,
+				},
+
+				{
+					Name:   "Height",
+					Value:  "1.0 m (3′03″)",
+					Inline: true,
+				},
+
+				{
+					Name:   "Weight",
+					Value:  "13.0 kg (28.7 lbs)",
+					Inline: true,
+				},
+			},
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, &ivysaur)
+	}
+
+	if m.Content == "nidorino" {
+		nidorino := discordgo.MessageEmbed{
+			Title:     "Nidorino",
+			Timestamp: time.Now().Format(time.RFC3339),
+			Color:     0xba55d3, //purple
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://archives.bulbagarden.net/media/upload/b/bb/033Nidorino_RG.png",
+			},
+
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "DexNr",
+					Value:  "0033",
+					Inline: true,
+				},
+
+				{
+					Name:   "Type",
+					Value:  "Poison",
+					Inline: true,
+				},
+
+				{
+					Name:   "Species",
+					Value:  "Poison Pin Pokémon",
+					Inline: true,
+				},
+
+				{
+					Name:   "Abilities",
+					Value:  "Overgrow/Rivalry/Hustle(Hidden Ablity)",
+					Inline: false,
+				},
+
+				{
+					Name:   "Height",
+					Value:  "0.9 m (2′11″)",
+					Inline: true,
+				},
+
+				{
+					Name:   "Weight",
+					Value:  "19.5 kg (43.0 lbs)",
+					Inline: true,
+				},
+			},
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, &nidorino)
+	}
+
 }
